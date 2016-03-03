f7966b98b34a33105e69e9c0d7829278e63765db
Jithu Sunny <jithusunnyk@gmail.com>
2016-03-03 14:46:01 +0530

    beta pushing alpha

---
+++ server.js
@@ -0,0 +1,76 @@
+//setup
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+mongoose.connect('mongodb://127.0.0.1:27017/todo_db');
+
+//configuration
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+//define model
+var Todo = mongoose.model('Todo', {
+	text : String
+});
+
+//routes
+//api
+
+//get all todos
+app.get('/api/todos', function(req, res) {
+	Todo.find(function(err, todos) {
+		if(err)
+			res.send(err);
+		res.json(todos);
+	});
+});
+
+
+app.post('/api/todos', function(req, res) {
+	Todo.create({
+		text: req.body.text,
+		done: false
+	}, function(err, todo) {
+		if (err)
+			res.send(err);
+
+		Todo.find(function(err, todos) {
+			if(err)
+				res.send(err)
+			res.json(todos);
+		});
+	});
+});
+
+ app.delete('/api/todos/:todo_id', function(req, res) {
+        Todo.remove({
+            _id : req.params.todo_id
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+    });
+
+app.get('*', function(req, res) {
+	res.sendfile('./public/index.html');
+});
+
+
+//listen
+app.listen(8080);
+console.log("App is now listening on port 8080");
+
